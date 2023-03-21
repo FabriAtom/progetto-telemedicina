f7f18e1db8abba7ea969317b316d10760d002da2
Fabrizio <fabrizioromano20@gmail.com>
2023-03-21 09:28:14 +0100

    first commit

---
+++ resources/js/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <section>
+            <div class="container">
+                <h1>{{ title }}</h1>
+            </div>
+        </section>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: 'Home page',
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/views/app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- il router view monta il componente Home.vue -->
+    <!-- rotta percorso e path sono in routes -->
+    <router-view></router-view>
+    <!-- <section>
+
+      <h1>questo è il titolo {{ title }}</h1>
+    </section> -->
+  </div>
+</template>
+
+<script>
+  // import PostCard from '../components/PostCard.vue'
+  export default {
+    // components: {
+    // },
+    data() {
+      return {
+        title : 'ciao',
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+
+</style>
